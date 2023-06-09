f768c23b2dce4f7ca40344dc65bce4e9d6464b58
Devesh Shakya <06deveshshakya@gamil.com>
2023-06-09 22:52:53 +0530

    added booking functionality

--- app.js
+++ app.js
@@ -1,39 +1,15 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
+// const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const Event = require("./models/event");
-const User = require("./models/user");
-const crypto = require("crypto-js");
+
 const schema = require("./graphql/schema/index");
 const resolvers = require("./graphql/resolver/index");
 
 app.use(bodyParser.json());
-
-const findEvents = async (eventIds) => {
-  const events = await Event.find({ _id: { $in: eventIds } });
-  return events;
-};
-
-const findUser = async (userId) => {
-  const user = await User.findById(userId);
-  // const promiseEvents = user.createdEvent.map(async (event) => {
-  //   const eventId = event.toString();
-  //   const eventDet = await findEvent(eventId);
-  //   return { ...eventDet._doc };
-  // });
-  // const allEvents = await Promise.all(promiseEvents);
-  // // console.log("EVENTS", allEvents);
-  // const userDet = { ...user._doc, createdEvent: allEvents };
-  // // console.log("USER_DET", userDet);
-  // return userDet;
-
-  const userEvents = await findEvents(user.createdEvent);
-  return { ...user._doc, createdEvents: userEvents };
-};
 
 app.use(
   "/graphql",
--- graphql/resolver/index.js
+++ graphql/resolver/index.js
@@ -1,6 +1,7 @@
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const crypto = require("crypto-js");
+const Booking = require("../../models/booking");
 
 const findEvents = async (eventIds) => {
   try {
@@ -12,6 +13,19 @@ const findEvents = async (eventIds) => {
   } catch (err) {
     throw err;
   }
+};
+
+const findOneEvent = async (eventId) => {
+  const event = await Event.findById(eventId);
+  const userId = event.creater.toString();
+  const creater = await findUser(userId);
+  // console.log("CREATOR", creater);
+  return {
+    ...event._doc,
+    _id: event.id,
+    date: new Date(event._doc.date),
+    creater: creater,
+  };
 };
 
 const findUser = async (userId) => {
@@ -109,6 +123,64 @@ module.exports = {
       // console.log("USER IS 2", user._doc);
       const { password, ...rest } = user._doc;
       return rest;
+    } catch (err) {
+      throw err;
+    }
+  },
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find();
+      return bookings.map(async (booking) => {
+        const eventId = booking._doc.event.toString();
+        const userId = booking._doc.user.toString();
+        const eventDetail = await findOneEvent(eventId);
+        const userDetail = await findUser(userId);
+        return {
+          ...booking._doc,
+          _id: booking.id,
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString(),
+          event: { ...eventDetail },
+          user: { ...userDetail },
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  bookEvent: async (args) => {
+    try {
+      const user = "645cb87a69e3cd84f92b5763";
+      const event = await Event.findById(args.eventId);
+      const booking = new Booking({ user, event });
+      const booked = await booking.save();
+      const eventDetail = await findOneEvent(args.eventId);
+      const userDetail = await findUser(user);
+      return {
+        ...booked._doc,
+        _id: booked.id,
+        createdAt: new Date(booked._doc.createdAt).toISOString(),
+        updatedAt: new Date(booked._doc.updatedAt).toISOString(),
+        event: { ...eventDetail },
+        user: { ...userDetail },
+      };
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
+  cancelBooking: async (args) => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate("event");
+      const userId = booking.event.creater.toString();
+      const userDetail = await findUser(userId);
+      const event = {
+        ...booking.event,
+        _id: booking.event.id,
+        creater: userDetail,
+      };
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
     } catch (err) {
       throw err;
     }
